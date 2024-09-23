48cbfac6c8138c7c7e507b6f826169e497606ad5
Florian <fdreschel@gmail.com>
2024-09-23 12:31:25 +0200

    Enlace archivos en subida y Token

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { Database } = require('@sqlitecloud/drivers');
 const express = require("express");
 const cors = require('cors')
 const app = express();
@@ -5,8 +6,25 @@ app.use(cors())
 const fs = require("fs");
 const ftp = require("ftp");
 const moment = require('moment');
+ 
+let database = new Database('sqlitecloud://cjajv32esz.sqlite.cloud:8860?apikey=pqXdLE9WN4KJaubEtPay1bpQJ4z6AkqNCBQuyu4Y8qc');
 
-function SubirArchivo(filePath, folder) {
+function EliminarArchivo(filePath) {
+    fs.unlink(filePath, (err) => { 
+        if (err) throw err;
+        console.log(`Archivo eliminado: ${filePath}`);
+    });
+}
+
+async function SubirArchivo(filePath, folder, userID, fileKey) {
+    if(!fileKey) {
+        console.log(`No se ha especificado una clave para el archivo ${filePath}`);
+
+        // Eliminar archivo temporal    
+        EliminarArchivo(filePath);
+        return;
+    } 
+    console.log(`Subiendo archivo: ${filePath} en ${folder} de usuario ${userID} + clave ${fileKey}`);
     let rutaArchivos = `./htdocs/uploads`;
     var c = new ftp();
     c.on("ready", function () { 
@@ -34,15 +52,24 @@ function SubirArchivo(filePath, folder) {
             // Almacenar archivo en la carpeta del usuario
             const destino = `${rutaArchivos}/${folder}/${fileName}`;
             console.log(`Subiendo archivo: ${fileName} en ${destino}`); 
-            c.put(filePath, destino, function (err) { 
+            c.put(filePath, destino, async function (err) { 
                 if (err) throw err;
                 console.log(`Archivo subido: ${fileName} (${destino})`); 
+
+                // Almacenar archivo en db
+                let result = await database.sql`USE DATABASE cs; 
+                INSERT INTO FILES (URL, METADATA) 
+                VALUES ("${folder}/${fileName}", "metapod")`;
+ 
+                let fileID = result.lastID; 
+            
+                // Crear relacion entre archivo y usuario en db
+                await database.sql`USE DATABASE cs; 
+                INSERT INTO ACCESS (FILE, USER, FILEKEY) 
+                VALUES (${fileID}, ${userID}, "${fileKey}")`;
                 
                 // Eliminar archivo temporal    
-                fs.unlink(filePath, (err) => { 
-                    if (err) throw err;
-                    console.log(`Archivo temporal eliminado: ${filePath}`);
-                });
+                EliminarArchivo(filePath);
             });
             c.end();
         }); 
@@ -68,50 +95,85 @@ const ValidarToken = (req, res, next) => {
                 message: 'Token inválido',
             });
         } else {
+            req.userID = payload.userID;
             req.user = payload.user;
             req.email = payload.email;
             next();
         }
     });
-}
-
-app.get("/test", (req, res) => {
-    res.status(200).json({
-        message: `ok!`,
-    });
-});
+} 
 
 const multer = require("multer");
 const upload = multer({dest:'./tmp/'});
-app.post("/upload", upload.single('upload'), (req, res) => { 
+app.post("/upload", ValidarToken, upload.single('upload'), async (req, res) => { 
     if (!req.file) {
         return res.status(400).json({ error: "No file uploaded" });
-    } else if (!req.headers["Authorization"]) {
-        //return res.status(400).json({ error: "No token" });
     }
-
-    let token = req.headers["Authorization"];
-    let username = "usuario";
-
+ 
+    let username = "usuario"; 
     let userFolder = username;
     fs.mkdir(userFolder, { recursive: true }, (err) => {
         if (err) throw err;
     });
     
-    SubirArchivo(`./${req.file.path}`, userFolder, req.file.originalname);
+    SubirArchivo(`./${req.file.path}`, userFolder, req.userID, req.fileKey);
 
     res.status(200).json({
         message: `File ${req.file.originalname} uploaded`,
     });
 });
 
-app.get("/", async (req, res) => {
-    //let results = database.sql`USE DATABASE cs; INSERT INTO usuarios (nombre) VALUES ("Paco"); SELECT * FROM usuarios`
-    //let results = await database.sql`USE DATABASE cs; SELECT * FROM usuarios`
-    //res.send(results)
+app.get("/insert_test", async (req, res) => { 
+    let results = await database.sql`USE DATABASE cs; 
+    INSERT INTO USERS (EMAIL, PASSWORD, SALT, PUBLICKEY) 
+    VALUES ("user@email.com", "asjiasijasdijadjioijoads", "sal", "1,2,3,4,5")`
     res.status(200).json({
-        message: `ok!`,
+        message: JSON.stringify(results),
     });
 });
+
+app.get("/delete_all", async (req, res) => { 
+    let results = await database.sql`USE DATABASE cs; 
+    DELETE * FROM TABLE ACCESS; DELETE * FROM TABLE FILES; DELETE * FROM TABLE USERS;`
+    res.status(200).json({
+        message: JSON.stringify(results),
+    });
+});
+
+// Obtener relaciones
+app.get("/access", async (req, res) => {
+    let results = await database.sql`USE DATABASE cs;
+    SELECT * FROM ACCESS`
+    res.status(200).json({
+        message: JSON.stringify(results),
+    });
+});
+
+// Obtener los archivos
+app.get("/files", async (req, res) => {
+    let results = await database.sql`USE DATABASE cs;
+    SELECT * FROM FILES`
+    res.status(200).json({
+        message: JSON.stringify(results),
+    });
+});
+
+// Obtener los usuarios + clave pública
+app.get("/users", async (req, res) => {
+    let results = await database.sql`USE DATABASE cs;
+    SELECT ID, EMAIL, PUBLICKEY FROM USERS`
+    res.status(200).json({
+        message: JSON.stringify(results),
+    });
+});
+
+app.get('/token', (req, res) => {  
+    let newToken = GenerarToken({
+        userID: 1,
+        user: 'florian', 
+        email: 'email@email.com',
+    });
+    res.status(200).json({ token: newToken });
+});    	 
 
 app.listen(3000);
